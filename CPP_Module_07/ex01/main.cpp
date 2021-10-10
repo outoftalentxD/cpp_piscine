2e3f0547641eac059b2137d61775f1c05288f568
melaena <melaena@student.21-school.ru>
2021-10-10 18:40:26 +0300

    started ex02

--- CPP_Module_07/ex01/main.cpp
+++ CPP_Module_07/ex01/main.cpp
@@ -9,7 +9,6 @@ void multiply(T& x, int multiplier)
 
 int main( void )
 {
-
 	int size = 5;
 	int tab[] = { 0, 1, 2, 3, 4 }; // <--- I never understood why you can't write int[] tab. Wouldn't that make more sense?
 	Awesome tab2[5];
