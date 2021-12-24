feb301b759d08822309644e68811854169786496
melaena <outoftalent12@gmail.com>
2021-12-24 16:59:16 +0300

    added test.sh to ex00 tests

---
+++ CPP_Module_00/ex00/test.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+./megaphone "shhhhh... I think the students are asleep..."
+echo "SHHHHH... I THINK THE STUDENTS ARE ASLEEP..."
+./megaphone Damnit " ! " "Sorry students, I thought this thing was off."
+echo "DAMNIT ! SORRY STUDENTS, I THOUGHT THIS THING WAS OFF."
+./megaphone
+echo "* LOUD AND UNBEARABLE FEEDBACK NOISE *"
